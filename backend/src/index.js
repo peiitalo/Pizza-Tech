c752c1d3fc6906500d8b556979a4c3ddf427ec7d
Pedro Italo <109381410+peiitalo@users.noreply.github.com>
2025-09-21 21:23:11 -0300

    feat(backend): adição do Dockerfile e configurações inicias do docker.

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,11 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 
-const PORTA= process.env.PORT || 3000;
+app.get('/', (req, res) => {
+  res.send('API está funcionando!');
+});
+
+const PORTA= process.env.PORT || 3030;
 
 app.listen(PORTA,() => {
   console.log(`Servidor está rodando na porta ${PORTA}`);
